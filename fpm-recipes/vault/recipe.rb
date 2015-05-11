55dcf69b785e4a99801a25d5bdde856bb8d12d14
darron froese <darron@froese.org>
2015-05-11 14:26:32 -0600

    Update to 0.1.2.

--- fpm-recipes/vault/recipe.rb
+++ fpm-recipes/vault/recipe.rb
@@ -1,13 +1,13 @@
 class Vault < FPM::Cookery::Recipe
   name 'vault'
 
-  version '0.1.1'
-  revision '2'
+  version '0.1.2'
+  revision '1'
   description 'Vault'
 
   homepage 'https://www.vaultproject.io/'
   source "https://dl.bintray.com/mitchellh/vault/vault_#{version}_linux_amd64.zip"
-  sha256 '856a9c89bada1b8007d85dd2ccd81bb19414691a7e16476282cf1acd920c484a'
+  sha256 '12c28cf7d6b6052c24817072fb95d4cfa2a391b507c705e960faf11afb5ee6ad'
 
   maintainer 'Darron Froese <darron@froese.org'
   vendor 'octohost'
